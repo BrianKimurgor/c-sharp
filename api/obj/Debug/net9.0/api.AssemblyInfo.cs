5ae89ea8ed68bb9b1ea9599f051628402d7d3a15
BrianKimurgor <kimurgorbrian20@gmail.com>
2025-05-21 14:13:52 +0300

    updates

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c38be376bcc0a06b726f3e2f1912c60cfe6ca5e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c44736bf65b51ec8fd3f970f2f9afd39b3f00da7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
