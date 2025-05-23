6ebd1b1cd95fd00ff0e197605f8ab49250140fa1
BrianKimurgor <kimurgorbrian20@gmail.com>
2025-05-23 11:21:25 +0300

    added other services

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8364e11542c028c9b6437ee489ac56625530b163")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99089a2dfef07e045ce65bcf7631367534c4348c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
